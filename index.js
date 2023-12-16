7bff0d1eea7b90a5ad5afc1ad93a26d422f6f27f
hakob-yan <devhakob@gmail.com>
2023-12-16 22:31:12 +0400

    fixed name

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ dotenv.config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const DB = require("./configs/db");
-const movie = require("./routes/movies");
+const movies = require("./routes/movies");
 DB.connect();
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api/movies", movie);
+app.use("/api/movies", movies);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("App listenong to 5000 port");
