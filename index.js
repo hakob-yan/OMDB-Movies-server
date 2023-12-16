2c70bc00a5224873eb78df794611bd11f5ff88ab
hakob-yan <devhakob@gmail.com>
2023-12-16 15:46:16 +0400

    initated db

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const DB = require("./configs/db");
@@ -8,6 +10,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("Alive");
+});
+
 app.listen(process.env.PORT || 5000, () => {
   console.log("App listenong to 5000 port");
+  console.log(process.env);
 });
