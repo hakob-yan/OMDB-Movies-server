52a941d5c90e995942806261d0ec03144fd31789
hakob-yan <devhakob@gmail.com>
2023-12-18 18:36:51 +0400

    seed movies

--- seeds/1_movies_seed.js
+++ seeds/1_movies_seed.js
@@ -1,28 +1,36 @@
 const { default: axios } = require("axios");
 const omdb = require("../services/omdb");
 const { OMDB_API } = require("../constants");
-const {modifyMovieArray} = require("../utils/modifyMovieArray");
+const { modifyMovieArray } = require("../utils/modifyMovieArray");
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-  await knex("movies").del();
-  const response = await axios.get(`${OMDB_API}&s=top`);
-  const movies = response.data.Search;
-  console.log(modifyMovieArray(movies));
-  await knex("movies").insert([
-    {
-      title: "Custom Movie",
-      year: "2025",
-      runtime: "112 min",
-      genre: "Action",
-      director: "Me",
-    },
-  ]);
-};
+  try {
+    await knex("movies").del();
+    const response = await axios.get(`${OMDB_API}&s=top`);
+    const movies = response.data.Search;
+    const moviesData = await Promise.all(
+      movies.slice(0, 10).map((el) => {
+        return omdb.getMovieById(el.imdbID || "");
+      })
+    );
 
-// is_favorite: true,
-// is_deleted: false,
-// user_id: 3,
+    const finalMoviesData = moviesData.map((el) => ({
+      title: el.Title || "",
+      year: el.Year || "",
+      runtime: el.Runtime || "",
+      genre: el.Genre || "",
+      director: el.Director || "",
+      image: el.iamge || "",
+      is_favorite: true,
+      is_deleted: false,
+      user_id: 3,
+    }));
+    await knex("movies").insert(finalMoviesData);
+  } catch (error) {
+    console.log(error);
+  }
+};
