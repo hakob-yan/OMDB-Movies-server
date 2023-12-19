50c747aa0710c5d65820a05063211a30671e41a0
Hakob Hakobyan <hakob@swaps.com>
2023-12-19 15:36:52 +0400

    added favorite

--- controllers/movies.js
+++ controllers/movies.js
@@ -93,7 +93,61 @@ module.exports = {
           args
         );
       }
-      await res.status(204).send("movie deleted");
+      //aslo can send 204 ,
+      await res.status(200).send(movieId);
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  },
+  updateMovieById: async (req, res) => {
+    try {
+      const { title, year, runtime, genre, director, is_favorite } =
+        req.body.data;
+      const userId = req.headers.authorization || "";
+      const { movieId } = req.params;
+
+      const userMovie = await pool.query(
+        "SELECT * FROM movies WHERE user_id = $1 AND imdb_id = $2",
+        [userId, movieId]
+      );
+      const foundMovie = userMovie.rows[0];
+      if (foundMovie) {
+        const updateMovie = await pool.query(
+          `UPDATE movies SET title = $3, year = $4, runtime = $5, genre = $6, director = $7, is_favorite = $8  WHERE user_id = $1 AND imdb_id = $2  RETURNING *`,
+          [
+            userId,
+            movieId,
+            title || foundMovie.title,
+            year || foundMovie.year,
+            runtime || foundMovie.runtime,
+            genre || foundMovie.genre,
+            director || foundMovie.director,
+            is_favorite !== undefined || foundMovie.is_favorite,
+          ]
+        );
+        res.status(200).send(updateMovie.rows[0]);
+      } else {
+        const movie = await omdb.getMovieById(movieId || "");
+        const modifiedMovie = modifyOmdbMovie(movie, userId);
+        const args = [
+          title || modifiedMovie.title,
+          year || modifiedMovie.year,
+          runtime || modifiedMovie.runtime,
+          genre || modifiedMovie.genre,
+          director || modifiedMovie.director,
+          modifiedMovie.image,
+          modifiedMovie.imdb_id,
+          is_favorite !== undefined || modifiedMovie.is_favorite,
+          modifiedMovie.is_deleted,
+          userId,
+        ];
+        const newMovie = await pool.query(
+          `INSERT INTO movies (title, year, runtime, genre, director, image, imdb_id, is_favorite, is_deleted, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8,$9, $10) RETURNING *`,
+          args
+        );
+        res.status(200).send(newMovie.rows[0]);
+      }
     } catch (error) {
       console.log(error);
       res.sendStatus(500);
--- routes/movies.js
+++ routes/movies.js
@@ -5,5 +5,6 @@ router.get("/all", controllers.movies.getAllMovies);
 router.get("/search", controllers.movies.getMoviesByTitle);
 router.get("/:movieId", controllers.movies.getMovieById);
 router.delete("/:movieId", controllers.movies.deleteMovieById);
+router.put("/:movieId", controllers.movies.updateMovieById);
 
 module.exports = router;
